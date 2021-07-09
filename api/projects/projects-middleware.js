0a54baeeedf38ce0bc4f1d6e66738571cddf8ebc
Ryan Ishimaru <rishimaru23@gmail.com>
2021-07-09 12:26:08 -0700

    wip

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,22 @@
-// add middlewares here related to projects
+// add middlewares here related to 
+const Project = require('./projects-model')
+
+async function validateProjectId(req,res, next){
+try{
+    const project = await Project.get(req.params.id)
+    if (!project){
+        res.status(404).json({ message : "This project couldn't be found"})
+    } else {
+        req.project = project
+        next()
+    }
+} catch (error){
+    next(error)
+    }
+}
+
+function validate
+
+module.exports = {
+    validateProjectId,
+}
