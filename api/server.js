16e09a171e737b763518a7b7b3aee0beca99241d
Ryan Ishimaru <rishimaru23@gmail.com>
2021-07-09 14:08:16 -0700

    all endpoints working besides one bug

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const server = express();
 const projectsRouter = require('./projects/projects-router')
+const actionsRouter = require('./actions/actions-router')
 
 server.use(express.json())
 
 server.use('/api/projects', projectsRouter)
+server.use('/api/actions', actionsRouter)
 
 server.get('/', (req,res) => {
     res.send(`<h1> test <h1>`)
